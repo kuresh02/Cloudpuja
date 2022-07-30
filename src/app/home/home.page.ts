b0896bfe054dda77cfae1ee9e68fc16aa6560603
kuresh <kuresh02@gmail.com>
2022-07-30 14:43:28 +0530

    Check

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+const userData = require('pirated-data');
+declare var require: any
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+ngOnInit(){
+  console.log("Helloworld");
+  var cardDetails = userData.financialCardGenerator();
+  console.log(cardDetails);
+}
   constructor() {}
 
 }
